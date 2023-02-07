346415a2b1198b3eed53e8796d2c0a1b40472093
marci <marcinmarion0@gmial.com>
2023-02-07 20:36:57 +0100

    zmiany

--- src/test/java/WebDriverMenager.java
+++ src/test/java/WebDriverMenager.java
@@ -16,6 +16,36 @@ public class WebDriverMenager {
         driver.navigate().to("https://testeroprogramowania.github.io/selenium/basics.html");
 
 
-        WebElement xPath = driver.findElement(By.xpath("/html/body/button")); // Podstawowa ścieżka lokalizacji
+        WebElement fullPath = driver.findElement(By.xpath("/html/body/div/ul")); // /html/body/div/button Podstawowa ścieżka lokalizacji
+        WebElement shortPath = driver.findElement(By.xpath("//ul")); //listy
+        WebElement buttonID = driver.findElement(By.xpath("//button[@id='clickOnMe']"));
+        WebElement firstName = driver.findElement(By.xpath("//input[@name='fname']"));
+        WebElement paraHidden = driver.findElement(By.xpath("//p[@class='topSecret']"));
+        WebElement input = driver.findElement(By.xpath("//input"));
+        WebElement linkText = driver.findElement(By.xpath("//a[text()='Visit W3Schools.com!']"));
+        WebElement partialLink = driver.findElement(By.xpath("//a[contains(text(),'Visit')]"));//zawiera teskst
+
+        //$x("(//input)[]") w nawiasie kwadratowym numer elementu
+        //$x("//*[@name]") wszystkie elementy które mają name
+        //$x("//*[@id]")
+        //$x("//button[@id]")
+
+        WebElement allXpath = driver.findElement(By.xpath("//*"));//całość
+        WebElement secondElement = driver.findElement(By.xpath("(//input)[2]"));//drugi element
+        WebElement lastElement = driver.findElement(By.xpath("(//input)[last()]"));
+        WebElement elementWithAttr = driver.findElement(By.xpath("//*[@name]"));
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
